fd65f3c2a5701e66562fcc196320350f9544cf23
TaeWonn <xodns9485@gmail.com>
2021-05-22 12:26:45 +0900

    YORIJORI-20

--- src/main/java/dev/ohjj/yorijori/api/YorijoriApiApplication.java
+++ src/main/java/dev/ohjj/yorijori/api/YorijoriApiApplication.java
@@ -1,5 +1,7 @@
 package dev.ohjj.yorijori.api;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,4 @@ public class YorijoriApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(YorijoriApiApplication.class, args);
     }
-
 }
